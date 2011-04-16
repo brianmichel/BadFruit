b529ed378acc2af42c1cceff602c88d5392454f5
Brian Michel <brian.michel@gmail.com>
2011-04-16 15:11:54 -0400

    We now return an array of Movie objects initialized with a BadFruit client instance as well as their movies.

--- lib/badfruit/Movies/movie.rb
+++ lib/badfruit/Movies/movie.rb
@@ -1,11 +1,12 @@
 class Movie
   attr_accessor :id, :name, :cast, :scores
 
-  def initialize(movieHash)
+  def initialize(movieHash, badfruit)
     @id = movieHash["id"]
     @name = movieHash["title"]
     @cast = movieHash["abridged_cast"]
     @scores = movieHash["ratings"]
+    @badfruit = badfruit
   end
 
   def averageScores
@@ -14,15 +15,15 @@ class Movie
   
   # for the three following methods, pass in your BadFruit client instance to retreive the info
   # this will be fixed when all results are parsed into movie assets for the user
-  def fullCast(badfruit)
-    return JSON.parse(badfruit.get_movie_info(@id, "cast"))
+  def fullCast
+    return JSON.parse(@badfruit.get_movie_info(@id, "cast"))
   end
   
-  def reviews(badfruit)
-    return JSON.parse(badfruit.get_movie_info(@id, "reviews"))
+  def reviews
+    return JSON.parse(@badfruit.get_movie_info(@id, "reviews"))
   end
   
-  def info(badfruit)
-    return JSON.parse(badfruit.get_movie_info(@id, "details"))
+  def info
+    return JSON.parse(@badfruit.get_movie_info(@id, "details"))
   end
 end
--- lib/badfruit/Movies/movies.rb
+++ lib/badfruit/Movies/movies.rb
@@ -10,7 +10,12 @@ module BadFruit
       if page_limit > 50
         page_limit = MAX_PAGE_LIMIT #current limitation of the rotten tomatos API
       end
-        JSON.parse(@badfruit.search_movies(name, page_limit, page))
+      movies = Array.new
+       data = JSON.parse(@badfruit.search_movies(name, page_limit, page))
+       data["movies"].each do |movie|
+         movies.push(Movie.new(movie, @badfruit))
+       end
+       return movies
     end
   end
 end
