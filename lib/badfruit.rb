5c36ce4c21656d3fad3fbd7efbf887a30be88fc2
Brooke McKim <brooke.mckim@gmail.com>
2011-04-17 08:48:50 -0400

    Made requires cross-OS compatible

--- lib/badfruit.rb
+++ lib/badfruit.rb
@@ -2,12 +2,12 @@ require 'httparty'
 require 'JSON'
 require 'cgi'
 
-require File.join(File.expand_path(File.dirname(__FILE__) + '/badfruit/base'))
-require File.join(File.expand_path(File.dirname(__FILE__) + '/badfruit/lists'))
-require File.join(File.expand_path(File.dirname(__FILE__) + '/badfruit/Movies/movies'))
-require File.join(File.expand_path(File.dirname(__FILE__) + '/badfruit/Movies/movie'))
-require File.join(File.expand_path(File.dirname(__FILE__) + '/badfruit/Reviews/review'))
-require File.join(File.expand_path(File.dirname(__FILE__) + '/badfruit/Actors/actor'))
+require File.join(File.expand_path(File.dirname(__FILE__)), 'badfruit', 'base')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'badfruit', 'lists')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'badfruit', 'Movies', 'movies')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'badfruit', 'Movies', 'movie')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'badfruit', 'Reviews', 'review')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'badfruit', 'Actors', 'actor')
 
 module BadFruit
   VERSION = "0.0.1"
