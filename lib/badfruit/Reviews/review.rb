5c4143624fc921dfc4ac22c62631cc882a67b966
Shaun Gehring <shaungehring@me.com>
2012-10-12 14:15:10 -0400

    Added Freshness to Reviews
    Added Freshness from the review json
    
    Conflicts:
    	lib/badfruit/Reviews/review.rb

--- lib/badfruit/Reviews/review.rb
+++ lib/badfruit/Reviews/review.rb
@@ -1,6 +1,6 @@
 module BadFruit
   class Review
-    attr_accessor :critic, :date, :publication, :quote, :links
+    attr_accessor :critic, :date, :publication, :quote, :links, :freshness
 
     def initialize(reviewHash)
       @critic = reviewHash["critic"]
@@ -8,6 +8,7 @@ module BadFruit
       @publication = reviewHash["publication"]
       @quote = reviewHash["quote"]
       @links = reviewHash["links"]
+      @freshness = reviewHash["freshness"]
     end
   end
 end
